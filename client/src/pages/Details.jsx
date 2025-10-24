54731910d88c1c44f1241782a88344a3a135f7ce
s-afrid <afrid0052@gmail.com>
2025-10-24 19:55:11 +0530

    designed the details page

--- client/src/pages/Details.jsx
+++ client/src/pages/Details.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import DetailsHeader from '../components/DetailsHeader'
 import PageDetails from '../components/PageDetails'
+import CardContainer from '../components/CardContainer'
+import Footer from '../components/Footer'
 
 const Details = () => {
   return (
@@ -9,6 +11,21 @@ const Details = () => {
         <Navbar />
         <DetailsHeader />
         <PageDetails />
+
+        <div className="bg-gray-50/50 py-16 px-4 sm:px-6 lg:px-8">
+      {/* Content wrapper with max-width for centering on large screens */}
+      <div className="max-w-7xl mx-auto">
+        <h2 className="text-3xl sm:text-4xl font-extrabold text-[#1F2937]"> {/* Dark blue/navy text color */}
+          Similar Website Examples
+        </h2>
+        <p className="mt-2 text-lg text-gray-700">
+          Explore stunning SaaS website designs for inspiration and creative ideas.
+        </p>
+      </div>
+    </div>
+
+        <CardContainer />
+        <Footer />
     </div>
   )
 }
